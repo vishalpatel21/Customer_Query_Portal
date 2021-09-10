700c040084a5a22d1c2a43261463e1bc4de52820
vishal patel <vishalpatel21@gmail.com>
2021-09-10 21:50:37 +0530

    add design pages

--- Customer_Query_Portal/ClientApp/src/App.js
+++ Customer_Query_Portal/ClientApp/src/App.js
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './custom.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import './custom.css'
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 
-export default class App extends Component {
-  static displayName = App.name;
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>positronX.io</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
 }
+
+export default App;
